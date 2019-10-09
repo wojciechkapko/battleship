5c708d5c73995717291537f711affb5ca6aeb410
Wojciech Kapko <wojciech.kapko@gmail.com>
2019-10-09 09:09:12 +0200

    Change shipTypes map initialization

--- Settings.java
+++ Settings.java
@@ -2,14 +2,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Settings {
-    private static Map<String, Integer> shipTypes = new HashMap<>();
+    private static Map<String, Integer> shipTypes = Map.of(
+        "Carrier", 5,
+        "Battleship", 4,
+        "Cruiser", 3,
+        "Submarine", 3,
+        "Destroyer", 2
+    );
 
     public static Map<String, Integer> getShipTypes() {
-        shipTypes.put("Carrier", 5);
-        shipTypes.put("Battleship", 4);
-        shipTypes.put("Cruiser", 3);
-        shipTypes.put("Submarine", 3);
-        shipTypes.put("Destroyer", 2);
         return shipTypes;
     }
 }
