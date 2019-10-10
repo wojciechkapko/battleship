6418ec3bc87e31ab032c9ad081cc3813b5444f79
marcinPluciennik <marcinpluciennik76@gmail.com>
2019-10-10 13:30:11 +0200

    add start menu

--- Main.java
+++ Main.java
@@ -1,6 +1,42 @@
+import java.util.Scanner;
+
 public class Main{
+    static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args){
-        GameCvC newGame = new GameCvC();
-        newGame.play();
+        menu();
+    }
+
+    private static void menu(){
+        String input = "";
+        while (!(input.equals("0"))){
+            System.out.println("Welcome");
+            System.out.println("1. Game: Player vs. Computer");
+            System.out.println("2. Game: Player vs. Player");
+            System.out.println("3. Game: Computer vs. Computer");
+            System.out.println("\nPress 0 to quit.");
+            
+            input = scan.next();
+
+            switch (input){
+                case "0":
+                   break;
+                case "1":
+                    GamePvC newGamePvC = new GamePvC();
+                    newGamePvC.play();
+                    break;
+                case "2":
+                    GamePvP newGamePvP = new GamePvP();
+                    newGamePvP.play();
+                    break;
+                case "3":
+                    GameCvC newGameCvC = new GameCvC();
+                    newGameCvC.play();
+                    break;
+                default:
+                System.out.println("Invalid input!");
+
+            }
+        }
     }
 }
